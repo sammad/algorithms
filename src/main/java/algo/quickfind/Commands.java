e37cf5aa202c74117ac784abd102b2db18b67354
Madhur Sambhar <madhurs@tpt.com>
2016-12-19 15:55:08 +0530

    Initial Commit

---
+++ src/main/java/algo/quickfind/Commands.java
@@ -0,0 +1,14 @@
+package algo.quickfind;
+
+public enum Commands {
+
+	SHOW("SHOW"),UNION("UNION"),CONNECTED("CONNECTED"),EXIT("EXIT"),HELP("HELP");
+	String type;
+	Commands(String type){
+		this.type=type;
+	}
+	public String getType() {
+		return type;
+	}
+
+}
