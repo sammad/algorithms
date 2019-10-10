1740fc8fdd6c24d0e4ace2c5223f1bedfb50ea15
Madhur Sambhar <madhur.sambhar@iongroup.com>
2019-10-10 19:55:22 +0530

    Corrected logic for finding parents and levels

--- src/main/java/com/ms/algo/graphs/BFSGraph.java
+++ src/main/java/com/ms/algo/graphs/BFSGraph.java
@@ -1,18 +1,22 @@
 package com.ms.algo.graphs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 public class BFSGraph {
 
 	private List<Integer> visitedVertices = new ArrayList<Integer>();
-	
+	private Map<Integer,Integer> parentMapping = new HashMap<>();
+	private Map<Integer,Integer> levelMapping = new HashMap<>();
 	public List<Integer> search(Graph graph, int sourceNode) {
 		Queue<Integer> vertexPendingQueue = new LinkedList<>();
 		vertexPendingQueue.add(sourceNode);
-		
+		parentMapping.putIfAbsent(sourceNode, -1);
+		levelMapping.putIfAbsent(sourceNode, 1);
 		while(!vertexPendingQueue.isEmpty()) {
 			Integer vertex = vertexPendingQueue.remove();
 			if(!visitedVertices.contains(vertex)) {
@@ -20,6 +24,8 @@ public class BFSGraph {
 				List<Integer> edges=graph.getEdges(vertex);
 				if(edges!=null) {
 					for(Integer neighbour:edges) {
+						parentMapping.putIfAbsent(neighbour, vertex);
+						levelMapping.putIfAbsent(neighbour, levelMapping.get(vertex)+1);
 						if(!vertexPendingQueue.contains(neighbour)) {
 							vertexPendingQueue.add(neighbour);
 						}
@@ -30,4 +36,11 @@ public class BFSGraph {
 		return visitedVertices;
 	}
 
+	public Map<Integer,Integer> getParentMapping(){
+		return parentMapping;
+	}
+	
+	public Map<Integer,Integer> getLevelMapping(){
+		return levelMapping;
+	}
 }
--- src/main/java/com/ms/algo/graphs/BFSTest.java
+++ src/main/java/com/ms/algo/graphs/BFSTest.java
@@ -8,7 +8,10 @@ public class BFSTest {
 		//(1,2) (1,3) (1,4)
 		Graph graph = Graph.createGraph(args);
 		graph.printGraphAdjacencyList();
-		List<Integer> visitedNodes = new BFSGraph().search(graph,9);
-		System.out.println(visitedNodes);
+		BFSGraph bfsGraph=new BFSGraph();
+		List<Integer> visitedNodes = bfsGraph.search(graph,1);
+		System.out.println("Visited Nodes: "+visitedNodes);
+		System.out.println("Parents Mapping: "+bfsGraph.getParentMapping());
+		System.out.println("Level Mapping: "+bfsGraph.getLevelMapping());
 	}
 }
